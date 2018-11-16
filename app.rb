b3fc1238a18b691d53350824151b29a2d95bf96d
leoncross <leon@lscross.co.uk>
2018-11-16 16:32:33 +0000

    removed global variables

--- app.rb
+++ app.rb
@@ -8,34 +8,39 @@ Capybara.app
 class Battle < Sinatra::Base
 enable :sessions
 
+  before do
+    @game = Game.instance
+  end
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_1 = Player.new(params[:player_1_name])
-    @player_2 = Player.new(params[:player_2_name])
-    $game = Game.new(@player_1, @player_2)
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    @game = Game.create(player_1, player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @game = $game
     erb :play
   end
 
   get '/attack_p1' do
-    @game = $game
-    @game.attack($game.player_2)
+    @game.attack(@game.player_2)
     erb :attack_p1
   end
 
   get '/attack_p2' do
-    @game = $game
-    @game.attack($game.player_1)
+    @game.attack(@game.player_1)
     erb :attack_p2
   end
 
+
+
+ # show switch turns
+ # during turn display own hitpoint
 
 
 
